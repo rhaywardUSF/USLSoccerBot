9f4ac17df35e6ae6fb9e4c4097ac0944dacb958d
Randy Hayward <yhayward@mail.usf.edu>
2017-05-19 18:07:41 -0400

    Remove Files

--- Helpers/PrintHelper.py
+++
@@ -1,9 +0,0 @@
-
-import time
-from time import gmtime, strftime
-
-def getTimeStr(): 
-    return time.strftime("%Y-%m-%d %H:%M:%S", gmtime())
-
-def tPrint(printMessage):
-    return print(getTimeStr() + " - " + printMessage)
